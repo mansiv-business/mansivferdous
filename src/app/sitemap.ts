779079a1001085ac6469195d722adbf7d7c81f9f
mansiv-business <mansivbusiness@gmail.com>
2026-01-20 16:05:06 +0600

    Update sitemap.ts

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -6,19 +6,25 @@ export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
       url: baseUrl,
-      lastModified: '2026-01-10', 
+      lastModified: '2026-01-20', 
       changeFrequency: 'monthly',
       priority: 1,
     },
     {
       url: `https://www.mansivferdous.com/blogs`,
-      lastModified: '2026-01-10',
+      lastModified: '2026-01-20',
       changeFrequency: 'weekly',
       priority: 0.8,
     },
     {
       url: `https://www.mansivferdous.com/blogs/website-pricing-breakdown-2026`,
-      lastModified: '2026-01-10',
+      lastModified: '2026-01-20',
+      changeFrequency: 'monthly',
+      priority: 0.6,
+    },
+    {
+      url: `https://www.mansivferdous.com/blogs/website-design-process`,
+      lastModified: '2026-01-20',
       changeFrequency: 'monthly',
       priority: 0.6,
     },
