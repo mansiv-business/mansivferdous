b3671dfab6c283261da04c46ad78bf423ca2a7c3
mansiv-business <mansivbusiness@gmail.com>
2026-01-25 10:20:13 +0600

    Update about.tsx

--- src/components/about/about.tsx
+++ src/components/about/about.tsx
@@ -1,12 +1,10 @@
-import * as motion from "motion/react-client"
-
 import styles from "./about.module.css"
-import { scaleRight } from "@/lib/animation"
+import Image from "next/image"
 
 const About = () => {
   return (
 
-    <section className={styles.aboutSection}>
+    <section id="about" className={styles.aboutSection}>
       <div className={styles.aboutDiv}>
         <h2>Information about me that you might want to know before hiring.</h2>
         <p>I’m Mansiv Al Ferdous.A Next.JS developer and UI/UX designer. My way of designing websites is far different than most. I don’t just focus on
@@ -15,8 +13,9 @@ const About = () => {
           clean and interactive websites that bring client clarity.</p>
       </div>
 
-      <motion.div className="div-hr" {...scaleRight} transition={{ duration: 0.6, type: "tween", ease: "easeIn"}}></motion.div>
-
+      <div className={styles.imageDiv}>
+        <Image src="/images/portfolioImg.png" alt="profile image" className={styles.heroImg} width={500} height={500} />
+      </div>
     </section>
   )
 }
