773fc7d000543b8f8de4220adea008d3bc7cb943
mansiv-business <mansivbusiness@gmail.com>
2026-01-26 18:44:12 +0600

    Update instaBrowser.tsx

--- src/components/instaBrowser.tsx
+++ src/components/instaBrowser.tsx
@@ -4,8 +4,36 @@ import { useEffect } from "react";
 
 export default function InstagramFontFix() {
   useEffect(() => {
-    if (/Instagram|FBAN|FBAV/i.test(navigator.userAgent)) {
+    const ua = navigator.userAgent;
+    const isInAppBrowser = /Instagram|FBAN|FBAV|FB_IAB|FB4A/i.test(ua);
+
+    if (isInAppBrowser) {
+
       document.documentElement.classList.add("insta-browser");
+
+
+      const hasAttempted = sessionStorage.getItem('redirect-attempted');
+      
+      if (!hasAttempted) {
+        sessionStorage.setItem('redirect-attempted', 'true');
+        
+        const currentUrl = window.location.href;
+
+        if (/Android/i.test(ua)) {
+
+          const intentUrl = `intent://${currentUrl.replace(/^https?:\/\//, '')}#Intent;end`;
+          window.location.href = intentUrl;
+        } else if (/iPhone|iPad|iPod/i.test(ua)) {
+
+          setTimeout(() => {
+            window.location.href = `x-safari-${currentUrl}`;
+          }, 100);
+          
+          setTimeout(() => {
+            window.open(currentUrl, '_blank');
+          }, 500);
+        }
+      }
     }
   }, []);
 
