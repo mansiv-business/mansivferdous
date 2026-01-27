95bb738222a65fc1d97ca2018e54a9e116b9881e
mansiv-business <mansivbusiness@gmail.com>
2026-01-27 17:38:24 +0600

    Update instaBrowser.tsx

--- src/components/instaBrowser.tsx
+++ src/components/instaBrowser.tsx
@@ -8,32 +8,7 @@ export default function InstagramFontFix() {
     const isInAppBrowser = /Instagram|FBAN|FBAV|FB_IAB|FB4A/i.test(ua);
 
     if (isInAppBrowser) {
-
       document.documentElement.classList.add("insta-browser");
-
-
-      const hasAttempted = sessionStorage.getItem('redirect-attempted');
-      
-      if (!hasAttempted) {
-        sessionStorage.setItem('redirect-attempted', 'true');
-        
-        const currentUrl = window.location.href;
-
-        if (/Android/i.test(ua)) {
-
-          const intentUrl = `intent://${currentUrl.replace(/^https?:\/\//, '')}#Intent;end`;
-          window.location.href = intentUrl;
-        } else if (/iPhone|iPad|iPod/i.test(ua)) {
-
-          setTimeout(() => {
-            window.location.href = `x-safari-${currentUrl}`;
-          }, 100);
-          
-          setTimeout(() => {
-            window.open(currentUrl, '_blank');
-          }, 500);
-        }
-      }
     }
   }, []);
 
