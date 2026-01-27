99b458ecba860165db9dc0de6e060e9cab1a6c16
mansiv-business <mansivbusiness@gmail.com>
2026-01-27 10:04:49 +0600

    Update about.tsx

--- src/components/about/about.tsx
+++ src/components/about/about.tsx
@@ -5,9 +5,9 @@ const About = () => {
   return (
 
     <section id="about" className={styles.aboutSection}>
-      <div className={styles.aboutDiv}>
+      <div className={`${styles.aboutDiv} p`}>
         <h2>Information about me that you might want to know before hiring.</h2>
-        <p>I’m Mansiv Al Ferdous.A Next.JS developer and UI/UX designer. My way of designing websites is far different than most. I don’t just focus on
+        <p>I’m Mansiv Al Ferdous. A Next.JS developer and UI/UX designer. My way of designing websites is far different than most. I don’t just focus on
           fancy UI but also UX so clients get the best experience when roaming at your site. My focus is
           simple, and that is to increase your leads through my service by generating SEO traffic and making
           clean and interactive websites that bring client clarity.</p>
