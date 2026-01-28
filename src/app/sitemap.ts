fe133ced5c53c5b0bf2a3cfe82dc84004ceb1971
mansiv-business <mansivbusiness@gmail.com>
2026-01-28 08:29:45 +0600

    Update sitemap.ts

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,30 +1,30 @@
 import { MetadataRoute } from 'next'
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = 'https://www.mansivferdous.com'
+  const baseUrl = 'https://mansivferdous.com'
 
   return [
     {
       url: baseUrl,
-      lastModified: '2026-01-20', 
+      lastModified: '2026-01-28', 
       changeFrequency: 'monthly',
       priority: 1,
     },
     {
-      url: `https://www.mansivferdous.com/blogs`,
+      url: `${baseUrl}/blogs`,
       lastModified: '2026-01-20',
-      changeFrequency: 'weekly',
+      changeFrequency: 'monthly',
       priority: 0.8,
     },
     {
-      url: `https://www.mansivferdous.com/blogs/website-pricing-breakdown-2026`,
+      url: `${baseUrl}/blogs/website-pricing-breakdown-2026`,
       lastModified: '2026-01-20',
       changeFrequency: 'monthly',
       priority: 0.6,
     },
     {
-      url: `https://www.mansivferdous.com/blogs/website-design-process`,
-      lastModified: '2026-01-20',
+      url: `${baseUrl}/blogs/website-design-process`,
+      lastModified: '2026-01-28',
       changeFrequency: 'monthly',
       priority: 0.6,
     },
