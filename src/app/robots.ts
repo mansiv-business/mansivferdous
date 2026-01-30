09c7056ee9bca5eeb60d77006e2617a95c19c142
mansiv-business <mansivbusiness@gmail.com>
2026-01-30 18:52:53 +0600

    Update robots.ts

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -6,6 +6,7 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: '*',
       allow: '/',
     },
-    sitemap: 'https://mansivferdous.com/sitemap.xml',
+    sitemap: 'https://www.mansivferdous.com/sitemap.xml',
   }
 }
+
